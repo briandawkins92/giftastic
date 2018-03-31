31b39cc2586b2a2d170759e2c3c9832a2e3e7dfa
Brian Dawkins <bkdawkins@gmail.com>
2018-03-31 12:29:10 -0700

    button works app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,25 +14,62 @@ function renderButtons (){
     $("#topics-view").append(button);
     console.log(button);
 }
-
 }
-
 renderButtons ();
 
+$("#add-topic").on("click", function (event){
+    event.preventDefault();
+    var input = $("#topics-input").val();
+    topics.push(input);
+    console.log(topics);
+    function renderButtons (){
+        $("#topics-view").text("");
+        for (i = 0; i < topics.length; i++) {
+        var button = $("<button>");
+        button.attr("class", "button");
+        button.attr("data-name", topics[i])
+        button.text(topics[i]);
+        $("#topics-view").append(button);
+        console.log(button);
+    }
+}
+renderButtons();
+showGifs ();
+})
+
+function showGifs () {
 $("button").on("click", function (){
 var button = $(this).attr("data-name");
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-button + "&api_key=4E6ZWhl3kjr7jApYIWgPeN5FvtUoAVIB";
+button + "&api_key=4E6ZWhl3kjr7jApYIWgPeN5FvtUoAVIB&limit=10";
 
 $.ajax ({
     url: queryURL,
     method: "GET"
 })
 .then(function(response){ 
-    
-})
+var results = response.data;
+
+for (var i=0; i < results.length; i++) {
+ var gifDiv = $("<div class='item'>");
+ var rating = results[i].rating;
+ var p = $("<p>").text("Rating:" + rating);
+
+ var personImage = $("<img>");
+
+ personImage.attr("src", results[i].images.fixed_height.url);
+
+ gifDiv.append(p);
+ gifDiv.append(personImage);
+ $("#gifs-appear-here").prepend(gifDiv);
+
+
+}
+
+});
 
 })
-
+}
+showGifs ();
 })
 /* END JAVASCRIPT*/
